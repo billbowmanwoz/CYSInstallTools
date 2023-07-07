155ed6dc925f1e5d6091585929f9d83899103f1a
billbowmanwoz <118026005+billbowmanwoz@users.noreply.github.com>
2023-07-07 15:48:02 -0400

    Create testing.ps1

---
+++ winget_option/testing.ps1
@@ -0,0 +1,9 @@
+﻿
+if (Get-ChildItem -Path Env:ChocolateyInstall) {
+choco upgrade chocolatey
+}
+else {
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+choco install winget
