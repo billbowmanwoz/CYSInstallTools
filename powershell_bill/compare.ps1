6e33bcdbcfc906d588bd9c05e5dd2c82419b77d9
billbowmanwoz <118026005+billbowmanwoz@users.noreply.github.com>
2023-07-14 17:26:01 -0400

    Update compare.ps1

--- powershell_bill/compare.ps1
+++ powershell_bill/compare.ps1
@@ -1,20 +1,21 @@
-$NameOfOS = Get-ComputerInfo -Property OSName
-$OSVersionRaw = Get-ComputerInfo -Property OSBuildnumber
-$regexPattern = "(?<=\=)(.*)$"
-$OSVer = [regex]::Match($OSVersionRaw, $regexPattern)
-if ($match.Success){
-    $result = $match.Groups[1].Value.Trim()
-    Write-Host "Extracted information: $result"
-}
+$systemInfo = systeminfo
 
-if ($NameOfOS -match "Windows 11") {
+$osRegex = $osRegex = 'OS Name:\s+(.+)$'
+$buildRegex = '(.+N/A Build )(.+)$'
+#$virtRegex_11 = 'Virtualization Enabled in Firmware'
+
+$osName = $systemInfo | Select-String -Pattern $osRegex | ForEach-Object { $_.Matches.Groups[1].Value }
+$OSBuildNumber = $systemInfo | Select-String -Pattern $buildRegex | ForEach-Object { $_.Matches.Groups[2].Value }
+#$virtEnabled = $systemInfo | Select-String  -Pattern 
+
+if ($osName -match "Windows 11") {
     Write-Host "Windows 11! YAY!"
-}elseif($NameOfOS -match "Windows 10") {
+}elseif($osName -match "Windows 10") {
     Write-Host "Windows 10"
-}
+    If ($OSBuildNumber -lt 17134){
+        Write-Host "NOSIR, No CYS for you!"
+    }else {
+        Write-Host "You can do it!"
+    }   
 
-If ($OSVersion -lt 17134){
-    Write-Host "NOSIR, No CYS for you!"
-}else {
-    Write-Host "You can do it!"
 }
