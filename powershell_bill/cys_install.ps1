040aaba047b3435054f0d4c2040a15455f1f304e
billbowmanwoz <william.bowman@learningsource.com>
2023-07-15 21:55:42 -0400

    Delete cys_install.ps1

--- powershell_bill/cys_install.ps1
+++
@@ -1,3 +0,0 @@
-Invoke-WebRequest -Uri "https://dl.dropboxusercontent.com/s/8hfx7cp4xn0jt5s/wget.exe" -UseBasicParsing -OutFile "wget.exe"
-
-Start-Process -FilePath 'cmd.exe' -ArgumentList '/c dir'
