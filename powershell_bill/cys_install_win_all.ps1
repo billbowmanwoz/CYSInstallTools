c0703db420d667483ae17e7ca3e1be34ede2f430
billbowmanwoz <william.bowman@learningsource.com>
2023-07-15 21:54:45 -0400

    Created HashCheck Funtion on OVA downloads

--- powershell_bill/cys_install_win_all.ps1
+++ powershell_bill/cys_install_win_all.ps1
@@ -1,9 +1,27 @@
-Write-Host -ForegroundColor Yellow "Setting Locale Variables for Installation"
+function UACPause {
+    Write-Host -ForegroundColor Red "Once You Press ENTER, please watch for a UAC Prompt to continue install"
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp")
+    Write-Host
+}
+function hashCheck {
+    $storedHash = Get-Content -Path $hashStored
+    Write-Host "Checking Hash Value of $fileToCheck using $hashStored file"
+    $calculatedHash = Get-FileHash -Algorithm SHA256 -Path $fileToCheck
+    if ($calculatedHash.Hash -eq $storedHash) {
+        Write-Host "Hash values match. File integrity verified."
+    } else {
+        Write-Host "Hash values do not match. File may have been altered."
+        Write-Host -ForegroundColor Black -BackgroundColor White "$fileToCheck does not pass hash check. You will need to re-download"
+    }}
+
+Write-Host -ForegroundColor Yellow "`n`n`n`n`n`nSetting Locale Variables for Installation`n"
 $currentUserPath = Resolve-Path ~
 $desktopPath = [Environment]::GetFolderPath('Desktop')
 $installFolder = "$desktopPath\CYS-Installer"
 $OVAfolder = "$installFolder\OVAs"
-Write-Host -ForegroundColor Yellow "Making sure certain locations exist"
+
+
+Write-Host -ForegroundColor Yellow "Making sure certain locations exist`n"
 $doesWingetExist = Test-Path -Path "$currentUserPath\AppData\Local\Microsoft\WindowsApps\winget.exe"
 $doesInstallerFolderExist = Test-Path -Path "$installFolder"
 $doesOVAFolderExist = Test-Path -Path "$OVAfolder"
@@ -11,44 +29,45 @@ $doesOVAFolderExist = Test-Path -Path "$OVAfolder"
 $appsWinget = @("zoom.zoom",
                 "SlackTechnologies.Slack",
                 "WiresharkFoundation.Wireshark",
-                "Oracle.VirtualBox",
+                "Oracle.VirtualBox -v 7.0.6",
                 "Microsoft.VisualStudioCode",
                 "python3",
                 "Google.Chrome")
 
-Write-Host -ForegroundColor Yellow "Setting download location to $desktopPath\CS-Installers"
+Write-Host -ForegroundColor Yellow "Setting download location to $installFolder`n"
 if(-not $doesInstallerFolderExist){
     New-Item -ItemType Directory -Path "$installFolder"
 }     
 Set-Location -Path "$installFolder"
 
-Write-Host -ForegroundColor Yellow "Checking to see if software is alreay installed."
+Write-Host -ForegroundColor Yellow "Checking to see if software is alreay installed.`n"
 if ($doesWingetExist) {
     Write-Host -ForegroundColor Yellow "Winget is already installed"
 } else {
-    Write-Host -ForegroundColor Yellow "Winget and Supporting Files Not installed - Installing"
-    Invoke-WebRequest -Uri "https://eternallybored.org/misc/wget/1.21.4/64/wget.exe" -Outfile wget.exe
+    Write-Host -ForegroundColor Yellow "Winget and Support Files Not installed - Installing`n"
+    Invoke-WebRequest -Uri "https://eternallybored.org/misc/wget/1.21.4/64/wget.exe" -UseBasicParsing -Outfile wget.exe
     .\wget --no-hsts --no-check-cert -N "https://aka.ms/vs/17/release/vc_redist.x64.exe"
-    Pause -ForegroundColor Red "About to Begin Visual C++ Redistributable Installation, Watch for the UAC prompt after pressing the ENTER key and allow the install"
+    UACPause
     Start-Process -FilePath ".\vc_redist.x64.exe" -ArgumentList "/quiet" -Wait
-    Invoke-WebRequest -Uri "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx" -UseBasicParsing -OutFile "$installFolder\Microsoft.VCLibs.x64.14.00.Desktop.appx"
+    .\wget --no-hsts --no-check-cert -N "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx"
+    .\wget --no-hsts --no-check-cert -N "https://github.com/microsoft/winget-cli/releases/download/v1.1.12653/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -O "WinGet.msixbundle"
     Add-AppxPackage "$installFolder\Microsoft.VCLibs.x64.14.00.Desktop.appx"
-    Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v1.1.12653/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "$installFolder\WinGet.msixbundle"
     Add-AppxPackage "$installFolder\WinGet.msixbundle"
 
-    Write-Host -ForegroundColor Yellow "You will need to close Powershell and re-run the original script to continue with this Installer."
-    Pause "Press ENTER to end"
+    Write-Host -ForegroundColor Red "You will need to close Powershell and re-run the original script to continue with this Installer."
+    Pause
     Exit
     }
 
-Write-Host -ForegroundColor Yellow "Installing applications for the discerning CYS Student"
+Write-Host -ForegroundColor Yellow "Installing applications for the discerning CYS Student`n"
 foreach ($app in $appsWinget){
     winget install $app
 }
+Write-Host -ForegroundColor Yellow "Installing apps that need special handling`n"
 
-winget install winpcap
-.\wget "https://nmap.org/dist/nmap-7.94-setup.exe"
-#winget install Insecure.Nmap
+.\wget --no-hsts --no-check-cert -N "https://nmap.org/dist/nmap-7.94-setup.exe"
+.\wget --no-hsts --no-check-cert -N "https://download.virtualbox.org/virtualbox/7.0.8/Oracle_VM_VirtualBox_Extension_Pack-7.0.8.vbox-extpack"
+
 
 Write-Host -ForegroundColor Yellow "Setting download location to $OVAfolder"
 if(-not $doesOVAFolderExist){
@@ -56,7 +75,22 @@ if(-not $doesOVAFolderExist){
 }     
 Set-Location -Path "$OVAfolder"
 
-..\wget --no-hsts --no-check-cert -N https://dl.dropbox.com/s/t8rqyornhedwt4e/kali-2023.ova
-..\wget --no-hsts --no-check-cert -N https://www.dropbox.com/s/zlk19cq2ued2ki3/kali-2023-sha.txt
+..\wget --no-hsts --no-check-cert -N https://dl.dropbox.com/s/t8rqyornhedwt4e/kali-2023.ova -O kali-2023.ova
+..\wget --no-hsts --no-check-cert -N https://dl.dropbox.com/s/zlk19cq2ued2ki3/kali-2023-sha.txt -O kali-2023-sha.txt
+..\wget --no-hsts --no-check-cert -N https://dl.dropboxusercontent.com/scl/fi/rj3aqa15mglb3v3r4byzu/pfSense-Router.ova?rlkey=qprrxsgwlorkxcxj96ars8dmo -O pfSense-Router.ova
+..\wget --no-hsts --no-check-cert -N https://dl.dropboxusercontent.com/scl/fi/9rlqa83hqlq5bftjioeo2/pfSense-Router.ova.sha?rlkey=i58ioubob5srkiscgvubymue3 -O pfSense-Router.ova.sha
+
+$ovaFileNames = @("kali-2023.ova",
+                "pfSense-Router.ova")
+$hashFilenames = @("kali-2023-sha.txt",
+                "pfSense-router.ova.sha")
+
+$length = $ovaFileNames.Length
+
+for ($i = 0; $i -lt $length; $i++) {
+        $hashStored = $hashFilenames[$i]
+        $fileToCheck = $ovaFileNames[$i]
+        hashCheck
+}
 
 Pause
