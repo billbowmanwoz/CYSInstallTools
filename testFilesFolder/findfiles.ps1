773b2cfa90f1d091886e21af645efa2af2b38157
billbowmanwoz <118026005+billbowmanwoz@users.noreply.github.com>
2023-07-20 11:09:24 -0400

    Update findfiles.ps1

---
+++ testFilesFolder/findfiles.ps1
@@ -0,0 +1,25 @@
+$appsWingetCheck = @("zoom.zoom",
+                    "SlackTechnologies.Slack",
+                    "WiresharkFoundation.Wireshark",
+                    "Oracle.VirtualBox",
+                    "TheDocumentFoundation.LibreOffice"
+                    "Microsoft.VisualStudioCode",
+                    "python3",
+                    "Google.Chrome")
+$appsWingetInstall = New-Object System.Collections.ArrayList 
+foreach ($app in $appsWingetCheck){
+    $doesAppExist = winget list $app
+    $match = [regex]::Match($doesAppExist, "No installed package found")
+    if ($match.Success) {
+        $appsWingetInstall += "$app"
+    }
+}
+
+$appsWingetInstall = New-Object System.Collections.ArrayList 
+foreach ($app in $appsWingetCheck){
+$doesAppExist = winget list $app
+$match = [regex]::Match($doesAppExist, "No installed package found")
+if ($match.Success) {
+    $appsWingetInstall += "$app"
+}
+}
