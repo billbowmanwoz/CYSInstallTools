6e32b37993d8a418a8efa7c394fc261078034036
billbowmanwoz <118026005+billbowmanwoz@users.noreply.github.com>
2023-07-24 19:04:44 -0400

    Update getComputerSpecs.ps1

--- testFilesFolder/getComputerSpecs.ps1
+++ testFilesFolder/getComputerSpecs.ps1
@@ -2,15 +2,22 @@ function getDriveSpecs {
     $mainDriveSize = Get-Volume -DriveLetter C
     $driveCapacityinGB = "{0:N2} GB" -f ($mainDriveSize.Size / 1GB)
     $driveCapacityLeftInGB = "{0:N2} GB" -f ($mainDriveSize.SizeRemaining / 1GB)
+    Write-Host "The Main Drive has a total size of $driveCapacityinGB"
+    Write-Host "And has $driveCapacityLeftInGB remaining"
 }
 
 
-function getCompSpec{
-    
+function getProc{
+    $ProcInfo = Get-CimInstance Win32_Processor
+    $procName = Select-Object Name
+    Write-Host "Processor Manufacturer: $ProcInfo.Manufacturer"
+    Write-Host "Processor Name:" $procName
+    Write-Host "Number of Cores / Logical Processors:+ "$ProcInfo.NumberofCores+ "/" + $ProcInfo.NumberOfProcessors +"`n"   
+    $ProcInfo | Select-Object Name, Manufacturer, NumberOfCores, NumberOfLogicalProcessors
+
 }
 
 
+getProc
 
 getDriveSpecs
-Write-Host "The Main Drive has a total size of $driveCapacityinGB"
-Write-Host "And has $driveCapacityLeftInGB remaining"
