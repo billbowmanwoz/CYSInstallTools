3ab8f69c5f115e68eaaf1558236bd7c4116fa223
billbowmanwoz <118026005+billbowmanwoz@users.noreply.github.com>
2023-07-24 10:16:29 -0400

    Create diskCapacity.ps1

---
+++ testFilesFolder/diskCapacity.ps1
@@ -0,0 +1,8 @@
+$mainDriveSize = Get-Volume -DriveLetter C
+
+$driveCapacityinGB = "{0:N2} GB" -f ($mainDriveSize.Size / 1GB)
+$driveCapacityLeftInGB = "{0:N2} GB" -f ($mainDriveSize.SizeRemaining / 1GB)
+
+
+Write-Host "The Main Drive has a total size of $driveCapacityinGB"
+Write-Host "And has $driveCapacityLeftInGB remaining"
