2296dd978d89193728b7b452728460cb08d1172f
Manuel powers <manuel.e.powers@gmail.com>
2023-12-14 18:59:49 -0600

    Making it gooder

--- ProgrammingFoundations/CSO106.ps1
+++ ProgrammingFoundations/CSO106.ps1
@@ -1,5 +1,4 @@
 $desktopPath = [System.Environment]::GetFolderPath("Desktop")
-
 $pythonFolder = Join-Path -Path $desktopPath -ChildPath "CSO106"
 New-Item -ItemType Directory -Path $pythonFolder
 
@@ -7,4 +6,10 @@ for ($i = 1; $i -le 10; $i++) {
     $lessonFolderName = "Lesson $i"
     $lessonFolderPath = Join-Path -Path $pythonFolder -ChildPath $lessonFolderName
     New-Item -ItemType Directory -Path $lessonFolderPath
+
+    $mainPyFilePath = Join-Path -Path $lessonFolderPath -ChildPath "main.py"
+    Set-Content -Path $mainPyFilePath -Value "# Your Python template for Lesson $i Hands-On assignment"
+
+    $additionalPyFilePath = Join-Path -Path $lessonFolderPath -ChildPath "practice.py"
+    Set-Content -Path $additionalPyFilePath -Value "# Practice Python template for Lesson $i"
 }
